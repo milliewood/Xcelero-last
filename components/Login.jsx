8b90a65b0866b5b9cdf5742a243048a657f199ae
milliewood <85115622+milliewood@users.noreply.github.com>
2023-08-12 12:18:22 +0300

    Update Login.jsx

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { signIn } from 'next-auth/react';
+import Image from 'next/image';
 
 const Login = () => {
   const handleGoogleSignIn = () => {
@@ -14,11 +15,12 @@ const Login = () => {
         className="px-4 py-2 border flex gap-2 border-slate-200 rounded-lg text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
         style={{ position: 'relative', top: '5rem', left: '-2.7rem' }}
       >
-        <img
-          className="w-6 h-6"
+        <Image
           src="https://www.svgrepo.com/show/475656/google-color.svg"
-          loading="lazy"
           alt="google logo"
+          width={24} // Adjust the width based on your design
+          height={24} // Adjust the height based on your design
+          loading="lazy"
         />
         <span>Sign in with Google</span>
       </button>
