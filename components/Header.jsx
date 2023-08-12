377977086965edec104d2c0cd6cd5216b6675b28
milliewood <85115622+milliewood@users.noreply.github.com>
2023-08-12 13:31:49 +0300

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -147,7 +147,7 @@ function Header() {
               {isAuthenticated && (
               <li>
                 <button onClick={toggleProductsModal} className="text-xs px-3 py-1 hover:text-[#a8bbcf] cursor-pointer" style={{ fontSize: '20px', display: 'flex', alignItems: 'center' }}>
-                  <svg className={`h-4 w-4 mr-2 ${session?.user ? 'text-gray-400'}`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <svg className={`h-4 w-4 mr-2 ${session?.user ?'text-white' :  'text-gray-400'}`} fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
                   </svg>
                   Products
