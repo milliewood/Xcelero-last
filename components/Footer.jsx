5c5d84e2bb61e054b64fb003e8bca308fe4a92a6
milliewood <85115622+milliewood@users.noreply.github.com>
2023-08-19 18:42:00 +0300

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 function Footer() {
   return (
+        <div>
+          {session?.user && (
+            <div className="lg:flex">
+              <p className="text-xs px-3 py-1 text-[#ffffff] lg:py-0 lg:px-0 lg:w-auto md:w-1/2 sm:w-full ml-[5rem]">
+                {session.user.email}
+              </p>
+            </div>
+          )}
+        </div>
     <div className='h-[20vh] z-[100] relative bg-black flex items-center justify-center text-center' >
         <a href='https://twitter.com/xcelero_' target="_blank" className='text-white text-2xl'>Powered by Xcelero</a>
     </div>
