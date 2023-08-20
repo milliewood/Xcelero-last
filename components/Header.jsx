752e9fb728e4010a695e6eb951d93cb4cfd67637
milliewood <85115622+milliewood@users.noreply.github.com>
2023-08-20 21:19:19 +0300

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -121,10 +121,14 @@ function Header() {
               <br/>
               
               <li>
-               <button onClick={toggleProductsModal} className="text-xs px-3 py-1 hover:text-[#a8bbcf] cursor-pointer" style={{ fontSize: '20px', display: 'flex', alignItems: 'center' }}>
-                Products
-              </button>
-              </li>
+              <button onClick={toggleProductsModal} className="text-xs px-3 py-1 hover:text-[#a8bbcf] cursor-pointer" style={{ fontSize: '20px', display: 'flex', alignItems: 'center' }}>
+              <svg className="h-4 w-4 mr-2 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
+              </svg>
+              Products
+            </button>
+
+            </li>
               <br/>
               <li>
               <button onClick={toggleFindUsModal} className="text-xs px-3 py-1 hover:text-[#959ba1] cursor-pointer" style={{ fontSize: '20px', display: 'flex', alignItems: 'center' }}>
